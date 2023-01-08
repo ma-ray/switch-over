81b12f2443652f6ca11ff9725540016348593b63
ma-ray <60418739+ma-ray@users.noreply.github.com>
2023-01-08 15:04:10 -0500

    remove console logs

--- extension/src/pages/options.js
+++ extension/src/pages/options.js
@@ -7,24 +7,21 @@ const signinId = document.getElementById('signin-id');
 // init
 chrome.runtime.sendMessage({ command: 'check-signin' }, (res) => {
   if (res) {
-    console.log('init signed in', res);
     signinSection.style.display = 'block';
     signoutSection.style.display = 'none';
     signinId.innerHTML = `You are signed in as ${res.email}`;
   } else {
     signinSection.style.display = 'none';
     signoutSection.style.display = 'block';
-    console.log('init signed out');
   }
 });
 
 signinbtn.onclick = (e) => {
   e.preventDefault();
   chrome.identity.getAuthToken({ interactive: true }, token => {
-    if (!token) console.log('some error');
+    if (!token) throw Error('Unable to get token');
     chrome.runtime.sendMessage({ command: 'sign-in', token }, (res) => {
       if (res) {
-        console.log('signin result', res);
         signinSection.style.display = 'block';
         signoutSection.style.display = 'none';
         signinId.innerHTML = `You are signed in as ${res.user.email}`;
@@ -35,10 +32,8 @@ signinbtn.onclick = (e) => {
 
 signoutbtn.onclick = (e) => {
   e.preventDefault();
-  console.log('options signing out');
   chrome.runtime.sendMessage({ command: 'sign-out' }, (res) => {
     if (res) {
-      console.log('signout sucess');
       signinSection.style.display = 'none';
       signoutSection.style.display = 'block';
     }
