a7a50245146941ce7fa2c8e7ae9edddca4044560
ma-ray <60418739+ma-ray@users.noreply.github.com>
2023-01-10 10:33:09 -0500

    extension: fix auth

--- extension/src/background.js
+++ extension/src/background.js
@@ -15,18 +15,8 @@ import { FIREBASE_CONFIG } from './firebase.config';
 import { getYoutubeLink } from './content/youtube';
 
 const app = initializeApp(FIREBASE_CONFIG);
-
-// FIREBASE DATABASE
-const db = getFirestore(app);
-
-// FIREBASE AUTH
 const auth = getAuth(app);
-
-let loading = true;
-
-chrome.runtime.onStartup.addListener(() => {
-  chrome.action.setBadgeText({ text: 'X' });
-});
+const db = getFirestore(app);
 
 // listeners for messages
 chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
@@ -54,35 +44,32 @@ chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
       });
     return true;
   } else if (message.command === 'check-signin') {
-    sendResponse(!loading ? auth.currentUser : null);
+    auth.onAuthStateChanged(user => {
+      sendResponse(user ? user.email : null);
+    });
   }
 });
 
-auth.onAuthStateChanged(user => {
-  if (user) {
-    // clicking the extension
-    chrome.action.setBadgeText({ text: '' });
-    loading = false;
-    const addLink = async(title, content) => {
-      return addDoc(collection(db, 'users', auth.currentUser.uid, 'links'), {
-        title,
-        userId: auth.currentUser.uid,
-        content,
-        createdAt: serverTimestamp(),
-      })
-    };
+const addLink = async(title, content, uid) => {
+  addDoc(collection(db, 'users', uid, 'links'), {
+    title,
+    userId: uid,
+    content,
+    createdAt: serverTimestamp(),
+  }).then(() => {
+    console.log('added');
+  });
+};
 
-    chrome.action.onClicked.addListener(async(tab) => {
-      if (tab.url.startsWith('https://') || tab.url.startsWith('http://')) {
-        if (tab.url.startsWith('https://www.youtube.com')) {
-          const res = await getYoutubeLink(tab.id, tab.url);
-          addLink(tab.title, res ?? tab.url);
-        } else {
-          addLink(tab.title, tab.url);
-        }
+chrome.action.onClicked.addListener(async(tab) => {
+  auth.onAuthStateChanged(async user => {
+    if (tab.url.startsWith('https://') || tab.url.startsWith('http://')) {
+      if (tab.url.startsWith('https://www.youtube.com')) {
+        const res = await getYoutubeLink(tab.id, tab.url);
+        addLink(tab.title, res ?? tab.url, user.uid);
+      } else {
+        addLink(tab.title, tab.url, user.uid);
       }
-    });
-  } else {
-    chrome.action.setBadgeText({ text: 'X' });
-  }
+    }
+  });
 });
