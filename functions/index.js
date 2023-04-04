55389d9d98682f4f467e6231a459e855a3e2fa17
ma-ray <60418739+ma-ray@users.noreply.github.com>
2023-04-04 00:52:18 -0400

    functions: add docs, minor improvement to addLink

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,17 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config());
 
+// When a new user is created through Firebase Auth, add a new user document to
+// Firestore.
 exports.newUser = functions.auth.user().onCreate((user) => {
-  // create new document on firestore
   return admin.firestore().collection("users").doc(user.uid).set({
     email: user.email,
     name: user.displayName,
   });
 });
 
+// When a new link is added for the user, create notification message to send
+// to userId's devices.
 exports.addLink = functions.firestore
     .document("users/{userId}/links/{linkId}")
     .onCreate(async (snap, context) => {
@@ -23,7 +26,7 @@ exports.addLink = functions.firestore
       functions.logger.log("User tokens", tokens);
       if (!tokens) {
         functions.logger.log("User has no devices.");
-        return new Promise(() => {});
+        return null;
       }
 
       const message = {
